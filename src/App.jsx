086c5493a7c7920ac97a9edd9b8c272dce287930
Uzair Tariq <uzair.t2006@gmail.com>
2025-07-06 20:53:04 +0500

    Completed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import gsap from "gsap"
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 import { ScrollTrigger, SplitText } from "gsap/all"
 
@@ -9,6 +10,8 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"/>
     </main>
   )
 }
