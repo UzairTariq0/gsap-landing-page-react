78c46b4fff844441355815690d333cb46a8f313b
Uzair Tariq <uzair.t2006@gmail.com>
2025-07-06 21:32:18 +0500

    Implemented About Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import gsap from "gsap"
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Cocktails from "./components/Cocktails"
+import About from "./components/About"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -10,7 +12,8 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"/>
+      <Cocktails />
+      <About />
     </main>
   )
 }
